903367a95114cdaf1de30af5c1efd0b828d78f01
tukibdma@gmail.com <91396589+chancindy845@users.noreply.github.com>
2021-10-23 11:29:30 -0500

    Update and rename database/menu/stayonscreen.js to base de datos/menú/stayonscreen.js

--- base de datos/menú/stayonscreen.js
+++ base de datos/menú/stayonscreen.js
@@ -1,10 +1,10 @@
 const stayonscreen = (prefix, ownerBot, botName) => {
         return `
-「 *${XavyBot}* 」
+「 *${Drag-nBot}* 」
 
 ◪ *INFO*
   ❏ Prefix: 「  ${prefix}  」
-  ❏ Creator: ${Ochoa}
+  ❏ Creator: ${Dragón}
 ◪ *ABOUT*
   │
   ├─ ❏ ${prefix}info
